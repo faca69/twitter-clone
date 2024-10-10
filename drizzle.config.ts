172d858c4259985e186c7c5acfac293311dcf2a3
faca69 <fnikolov123456789@gmail.com>
2024-10-10 13:35:53 +0200

    realtions semi done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   schema: [
     "./src/db/schemas/tweet.schema.ts",
     "./src/db/schemas/user.schema.ts",
+    "./src/db/schemas/users_follows.schema.ts",
+    "./src/db/schemas/users_liked_tweets.ts",
   ],
   out: "",
   dialect: "postgresql",
@@ -11,7 +13,7 @@ export default defineConfig({
     host: "localhost",
     user: "postgres",
     password: "postgres",
-    database: "twitter_clone",
+    database: "twitter",
     port: 5432,
   },
   verbose: true,
