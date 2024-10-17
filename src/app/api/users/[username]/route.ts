00f6817c5307ca86eb51abf760c89458aa6528f9
faca69 <fnikolov123456789@gmail.com>
2024-10-17 18:53:39 +0200

    fix 4

--- src/app/api/users/[username]/route.ts
+++ src/app/api/users/[username]/route.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest } from "next";
 import { getUserByUsername } from "../../../../services/users.service";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: NextApiRequest,
+  req: NextRequest,
   { params: { username } }: { params: { username: string } }
 ) {
   const user = await getUserByUsername(username);
